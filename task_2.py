c169cba9d4bca45191aa916c89dbc3a70f18dc50
sashakudriavtsew-design <sasha.kudriavtsew@yandex.ru>
2025-12-17 00:13:08 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,20 +1,13 @@
-salary = 5000
-spend = 6000
-months = 10
-increase = 0.03
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+x = 0
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+for i in (range(months)):
+    a = spend * (1 + increase)**i
+    x = x + a - salary
 
-money_capital = 0
-while True:
-    capital = money_capital
-    current_spend = spend
-    for r in range(months):
-        if capital + salary < current_spend:
-            break
-        capital = capital + salary - current_spend
-        current_spend *= (1 + increase)
-    else:
-        print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
-        break
-    money_capital += 1
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(x))
